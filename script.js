a0b3f840471dcffe5dc1fc371372fdaa5e4ba0a2
Tulani <87509369+TulaniSouza@users.noreply.github.com>
2023-12-08 22:34:52 -0300

    Primeira versão

---
+++ script.js
@@ -0,0 +1,42 @@
+let reveillon= "Ano novo está chegando!";
+let cantora="Mariah Carey";
+let cantor="Roberto Carlos";
+let anoAtual=2023;
+const nascimento=1969;
+let idade="54";
+
+//console.log( anoAtual - nascimento)
+
+const dataNascimento="27 de março de 1969"
+
+let results= `O ${reveillon}  e  com isso os cantores mais cobiçados no natal:
+${cantora} no auge de seus ${idade} anos e o Rei ${cantor} estarão juntos em nosso 
+Cruzeiro Marítmo pela Costa Brasileira por 5 dias incríveis. Não perca essa grande oportunidade ligue para 97070-7070, 
+se não conseguir, 70 de novo. Até lá!`;
+console.log(results)
+
+
+let diaDaSemana="Quarta-feira"
+//console.log(diaDaSemana)
+
+let assistir= 17;
+if (assistir >=18){ 
+    console.log("Você é maior de idade, poderá curtir o show do Roberto Carlos com participação especial de Mariah Carey.") 
+}else{
+    console.log("Sinto muito, mas assistirá da tv globo") 
+}
+
+
+let mes= ("Agosto" ||"Dezembro"||"Junho")
+if ((mes==="Agosto") ||(mes==="Dezembro")||(mes==="Junho")){
+    console.log("Uma ou mais opções estão corretas. O mês escolhido foi o mês")
+} else{
+    console.log("Algo de errado não está certo, o mês digitado foi o mês ")
+}
+
+
+
+
+
+
+    
